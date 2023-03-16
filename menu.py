1b2c56c87fbb2bcdc263a5e17b1e06c1a747d156
annamariaqa <126241502+annamariaqa@users.noreply.github.com>
2023-03-16 18:51:15 +0200

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,1 +1,13 @@
+import random
+from random import randint
 
+dinnerNames = input('введіть список страв до вечері: ')
+
+dinnerList = dinnerNames.split(', ')
+
+total = 0
+for dish in dinnerList:
+    price = random.randint(100, 200)
+    total = total + price
+    print(dish + '.......' + str(price) + 'грн')
+print('всього' + '.......' + str(total) + 'грн')
